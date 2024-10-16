c3193f9a3c9138e9b2cda83ad9de5a883df995f1
Ekaterina <kisaiksa@yandex.ru>
2024-10-16 21:06:04 +0300

    Спринт 15. Техническое задание. Часть 3.

--- server/src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
+++ server/src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.request.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.request.model.ItemRequest;
 
@@ -12,8 +11,5 @@ public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> 
     List<ItemRequest> findAllByRequester_IdOrderByCreatedDesc(Long userId);
 
     List<ItemRequest> findAllByOrderByCreatedDesc();
-
-//    @Query("SELECT ir FROM ItemRequest ir LEFT JOIN FETCH ir.items WHERE ir.id = ?1")
-//    List<ItemRequest> findRequestWithItems(Long requestId);
 
 }
